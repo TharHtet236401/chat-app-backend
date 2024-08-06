dec1eed334f27b3237d2652cbab94a7342ff8a23
TharHtet236401 <tharhteta3@gmail.com>
2024-08-06 22:11:48 +0700

    before adding jwt token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,55 @@
-export const signup = (req, res) => {
-    res.send('Signup route');
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+
+
+export const signup = async (req, res) => {
+    try {
+        const {fullName,username,password,confirmPassword,gender} = req.body;
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: 'Passwords do not match' });
+        }
+        let user = await User.findOne({username});
+        if (user) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        
+        //hash password//
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+        
+        let boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        let girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+    
+        const newUser = new User(
+            {
+                fullName,
+                username,
+                password: hashedPassword,
+                gender,
+                profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
+            }
+        );
+
+        if (newUser) {
+            await newUser.save();
+            res.status(201).json({
+                _id :newUser._id,
+                fullName:newUser.fullName,
+                username:newUser.username,
+                gender:newUser.gender,
+                profilePic: newUser.profilePic
+            });
+        }else{
+            return res.status(400).json({ message: 'User not created' });
+        }
+
+       
+        
+
+    } catch (error) {
+        console.log("Error in signup",error.message);
+        res.status(500).json({ errorMessage: "Internal Server Error"});
+    }
 };
 
 export const login = (req, res) => {
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config();
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-import authRoutes from './routes/auth.routes.js';
+dotenv.config();
 
-app.use(express.json());
+app.use(express.json());//middleware to parse the request body as json
+
 app.use('/api/auth', authRoutes);
+
+
+
+
+
+
 
 app.get("*", (req, res) => {
     res.send("404 Not Found");
 });
 
 app.listen(PORT, () => {
+    connectToMongoDB();
     console.log(`Server is running on port ${PORT}`);
 });
