4eaca1649a9584399f914894517036a1b5392213
TharHtet236401 <tharhteta3@gmail.com>
2024-08-13 22:00:40 +0700

    added the chat.js not sure need or not

---
+++ socket/chat.js
@@ -0,0 +1,63 @@
+
+
+export const liveUser = async( socketId,user) =>{
+    user ['socketId'] = socketId;
+    await Redis.setObj(socketId,user._id);
+    await Redis.setObj(user._id,user);
+    
+}
+
+export const initialize = async(io, socket) =>{
+    socket.emit("greet",{"name":socket.data.name,"id":socket.data._id})
+    socket.emit ("socketId",socket.id)
+    socket['currentUserId'] = socket.data._id;
+    liveUser(socket.id,socket.data)
+    socket.on('unread',(data)=> sendUnreadMsg(socket));
+    socket.on ('message',(data)=> incomingMessage(socket,data,io))
+    socket.on('load-more',(data)=> loadMore(socket,data,io))
+}
+
+export const sendUnreadMsg = async(socket) =>{
+    let unreads = await UnReadTB.find({to:socket.currentUserId});
+    console.log(unreads.length)
+    if (unreads.length > 0){
+        for (let unread of unreads){
+            await UnReadTB.findOneAndDelete(unread._id);
+        }
+    }
+  
+    socket.emit ("unread", {unreads:unreads.length})
+}
+
+export const incomingMessage = async(socket,message,io) =>{
+    
+    let msg = await new MessageTB(message).save();
+    let msgResult = await MessageTB.findById(msg._id).populate('from').populate('to');
+    let toUser =await Redis.getObj(message.to);
+  
+    
+    if(toUser){
+       let toSocket = io.of("/chat").sockets.get(toUser.socketId);
+       if(toSocket){
+        toSocket.emit("message",msg)
+       }else{
+        await new UnReadTB({from:message.from,to:message.to}).save();
+       }
+    }else{
+        await new UnReadTB({from:message.from,to:message.to}).save();
+    }
+    
+   
+    
+    
+}
+
+export const loadMore = async(socket,data,io) =>{
+    let limit = Number(process.env.MSG_LIMIT);
+    let skip = data.page == 0?0:Number(data.page)*limit;
+    let messages = await MessageTB.find({
+        $or :[{from:socket.currentUserId,},{to:socket.currentUserId}]
+    }).sort({created:-1}).skip(skip).limit(limit).populate('from to','name _id');
+    socket.emit("message",messages)
+}
+
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,24 +1,30 @@
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 
+// This function generates a JSON Web Token (JWT) for a given user_id and sets it as a cookie in the response.
 export const generateTokenAndSetCookie = (res, user_id) => {
+    // Sign a JWT with the user_id and a secret key, set to expire in 30 days.
     const token = jwt.sign({ id: user_id }, process.env.JWT_SECRET, {
         expiresIn: '30d',
     });
+    // Set the JWT as a cookie in the response, with security features to prevent XSS and ensure it's sent over HTTPS in development.
     res.cookie('jwt', token, {
         httpOnly: true, // prevent XSS
         sameSite: 'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000,
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
         secure: process.env.NODE_ENV === 'development',
     });
 };
 
 
+// This function generates a JWT token with a payload and an expiration time of 24 hours.
 export const genToken = (payload) => jwt.sign({
     exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
     data: payload
 }, process.env.JWT_SECRET)
 
+// This function extracts the token from the socket handshake and verifies it.
+// If the token is valid, it sets the current user in the socket.
 export const tokenFromSocket = async (socket,next)=>{
     let user = "blank"
     let token = socket.handshake.query.token
